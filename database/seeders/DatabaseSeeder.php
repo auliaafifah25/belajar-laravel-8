f530d28a887a7ece02cfb3239b204d7059495ab4
Au Lia <aulia2203lia@gmail.com>
2024-01-29 10:58:49 +0700

    test database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+        $faker = Faker::create('id_ID');
+
+        for($i =1; $i <= 5; $i++) {
+            DB::table('students')->insert([
+                'name' => $faker->name,
+                'score' => $faker->numberBetween(0,100)
+            ]);
+        }
     }
 }
